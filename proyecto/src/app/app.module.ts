1ef7647fbafa23555180a1359efe4b95fed1b4cb
JuanBircher <106487918+JuanBircher@users.noreply.github.com>
2022-06-10 20:45:07 -0300

    Desarrollo a continuar de Header, Banner, Logo

---
+++ proyecto/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    RedesComponent,
+    BannerComponent,
+    SobremiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
