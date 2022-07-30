bf64014de7cc9d27a2858fefc255fdd893142fec
JuanBircher <106487918+JuanBircher@users.noreply.github.com>
2022-07-30 02:59:40 -0300

    Servicios Creados

--- proyecto/BackEnd/juanbircher/src/main/java/com/proyectointegrador/juanbircher/Controller/PersonaController.java
+++ proyecto/BackEnd/juanbircher/src/main/java/com/proyectointegrador/juanbircher/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.proyectointegrador.juanbircher.Entity.Persona;
 import com.proyectointegrador.juanbircher.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,18 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
-    
+
     @GetMapping("personas/traer/perfil")
     public List<Persona> getPersona(){
     return ipersonaService.getPersona();
             }
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("personas/crear")
     public String createPersona(@RequestBody Persona persona){
      ipersonaService.savePersona(persona);
      return "La persona fue creada correctamente";
     }
-    
+
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("personas/borrar/(id)")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
@@ -37,6 +40,7 @@ public class PersonaController {
 }
     
     //URL:PUERTO/personas/editar/4/nombre % apellido % img //
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("personas/editar/(id)")
     public Persona editPersona (@PathVariable Long id,
                                 @RequestParam("nombre") String nuevoNombre,
--- proyecto/BackEnd/juanbircher/src/main/java/com/proyectointegrador/juanbircher/Security/jwt/JwtProvider.java
+++ proyecto/BackEnd/juanbircher/src/main/java/com/proyectointegrador/juanbircher/Security/jwt/JwtProvider.java
@@ -3,7 +3,6 @@ package com.proyectointegrador.juanbircher.Security.jwt;
 import com.proyectointegrador.juanbircher.Security.Entity.UsuarioPrincipal;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.SignatureException;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
