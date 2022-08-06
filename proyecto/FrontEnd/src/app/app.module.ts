3ec4e636f08953a1a62a0eb7a53d02c643c385dd
JuanBircher <106487918+JuanBircher@users.noreply.github.com>
2022-08-06 18:57:55 -0300

    Interceptor works

--- proyecto/FrontEnd/src/app/app.module.ts
+++ proyecto/FrontEnd/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import {FormsModule} from "@angular/forms";
+import {interceptorProvider} from "./service/interceptor-service";
+
 
 @NgModule({
   declarations: [
@@ -40,7 +42,10 @@ import {FormsModule} from "@angular/forms";
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ]
+  ,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
