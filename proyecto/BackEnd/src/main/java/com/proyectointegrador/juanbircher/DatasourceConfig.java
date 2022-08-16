9559ee99b8391f821a79b5abf836fc219e5a3641
JuanBircher <106487918+JuanBircher@users.noreply.github.com>
2022-08-15 22:08:46 -0300

    last changes

--- proyecto/BackEnd/src/main/java/com/proyectointegrador/juanbircher/DatasourceConfig.java
+++ proyecto/BackEnd/src/main/java/com/proyectointegrador/juanbircher/DatasourceConfig.java
@@ -12,7 +12,7 @@ public class DatasourceConfig {
     public DataSource datasource(){
     return DataSourceBuilder.create()
             .driverClassName("com.mysql.cj.jdbc.Driver")
-            .url("jdbc:mysql://ubtpdf9rnevkim2s:OLFrBzQjImAYr3nuand5@btqo9pspy889pu5qcpfp-mysql.services.clever-cloud.com:3306/btqo9pspy889pu5qcpfp")
+            .url("jdbc:mysql://btqo9pspy889pu5qcpfp-mysql.services.clever-cloud.com:3306/btqo9pspy889pu5qcpfp")
             .username("ubtpdf9rnevkim2s")
             .password("OLFrBzQjImAYr3nuand5")
             .build();
