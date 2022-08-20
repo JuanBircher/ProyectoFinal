381ebc8ec158f619d9da9690a84428bb2cfcff8b
JuanBircher <106487918+JuanBircher@users.noreply.github.com>
2022-08-20 15:36:15 -0300

    All works

--- proyecto/BackEnd/src/main/java/com/proyectointegrador/juanbircher/DatasourceConfig.java
+++ proyecto/BackEnd/src/main/java/com/proyectointegrador/juanbircher/DatasourceConfig.java
@@ -7,16 +7,10 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatasourceConfig {
-    //LOCAL
-    private final String DB_HOST_URI = "jdbc:mysql:mysql://ubtpdf9rnevkim2s:OLFrBzQjImAYr3nuand5@btqo9pspy889pu5qcpfp-mysql.services.clever-cloud.com:3306/btqo9pspy889pu5qcpfp";
-    private final String DB_PASSWORD="OLFrBzQjImAYr3nuand5";
-    private final String DB_USER="ubtpdf9rnevkim2s";
 
-    /*Productive
-    private final String DB_HOST_URI = "jdbc:mysql://btqo9pspy889pu5qcpfp-mysql.services.clever-cloud.com:3306/btqo9pspy889pu5qcpfp" +
+    private final String DB_HOST_URI = "jdbc:mysql://btqo9pspy889pu5qcpfp-mysql.services.clever-cloud.com:3306/btqo9pspy889pu5qcpfp";
     private final String DB_PASSWORD="OLFrBzQjImAYr3nuand5";
     private final String DB_USER="ubtpdf9rnevkim2s";
-   */
 @Bean
     public DataSource datasource(){
     return DataSourceBuilder.create()
